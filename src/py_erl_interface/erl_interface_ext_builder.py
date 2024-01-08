3d2d9662bcf0f113744cf4134c9f78d2a3c90e8c
Alexander Dodson <dodsonn@gmail.com>
2024-01-08 00:56:07 -0500

    use pycparser(ext) to process ei.h automatically to generate a file suitable for passing to ffi.cdef()

--- src/py_erl_interface/erl_interface_ext_builder.py
+++ src/py_erl_interface/erl_interface_ext_builder.py
@@ -1,9 +1,49 @@
 import os
+import logging
 from cffi import FFI
+from pycparser import parse_file
+from pycparserext.ext_c_generator import GnuCGenerator
+from pycparserext.ext_c_parser import GnuCParser
 
+logger = logging.getLogger(__name__)
 
 OTP_PATH = os.getenv('PYEI_OTP_PATH')
-CDEF_PATH = os.getenv('PYEI_CDEF_PATH') or os.path.join(os.path.dirname(__file__), 'pyei.cdef')
+CPP_PATH = os.getenv('PYEI_CPP_PATH') or 'cpp'
+
+
+def _should_filter_ast_node(node) -> bool:
+    def from_eih_file(n):
+        return n.coord and 'ei.h' in n.coord.file
+    
+    def is_erl_errno(n):
+        # this one is a hack because I didn't see a clear way to distinguish 
+        # plain var decls from other decls in the AST. The cdef only needs to
+        # have the typedefs and functions in it, just drop this node entirely.
+        # If other versions of erl_interface have other such declarations then
+        # this will need to become less of a hack.
+        return n.name == '__erl_errno'
+        
+    return from_eih_file(node) and not is_erl_errno(node)
+    
+def try_generate_cdef():
+    eih_path = os.path.join(OTP_PATH, 'include', 'ei.h')
+    ast = parse_file(eih_path, use_cpp=True, cpp_path=CPP_PATH, parser=GnuCParser())
+    filtered_children = [child for child in ast.ext if _should_filter_ast_node(child)]
+    ast.ext = filtered_children
+    c_gen = GnuCGenerator()
+    pyei_path = os.path.join(os.path.dirname(__file__), 'pyei.h')
+    with open(pyei_path, 'w') as cdef_f:
+        cdef_f.write(c_gen.visit(ast))
+    return pyei_path
+
+CDEF_PATH = os.getenv('PYEI_CDEF_PATH')
+
+if not CDEF_PATH:
+    try:
+        CDEF_PATH = try_generate_cdef()
+    except Exception as e:
+        logger.error('Failed to process ei.h automatically, falling back to bundled cdef', exc_info=True)
+        CDEF_PATH = os.path.join(os.path.dirname(__file__), 'pyei.cdef')
 
 builder = FFI()
 
